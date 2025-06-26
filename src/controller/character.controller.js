5d4999463ab143f18e086d46fdf86c28079b0ca0
¨lautaro <shortlauty35@gmail.com>
2025-06-26 16:47:10 -0300

    se añadieron las funciones con respecto a los campos nulos y la verificacion del tipo de ki

--- src/controller/character.controller.js
+++ src/controller/character.controller.js
@@ -1,12 +1,39 @@
+import { INTEGER, STRING } from "sequelize";
 import Character from "../models/Character.model.js";
+//verificacion de que los atributos no sean validos
 
 export const createcharacter = async (req, res) => {
+    const{ id, name, ki, gender, race, descripcion} = req.body;
+    if (name===""){
+        return res.status(401).json({message: "no se permiten campos vacios"})
+    };
+    if (id===""){
+        return res.status(401).json({message: "no se permiten campos vacios"})
+    };
+    if (ki===""){
+        return res.status(401).json({message: "no se permiten campos vacios"})
+    };
+    if (gender===""){
+        return res.status(401).json({message: "no se permiten campos vacios"})
+    };
+    if (race===""){
+        return  res.status(401).json({message: "no se permiten campos vacios"})
+    };
+    //verificacion del ki para que sea entero
+
+    const kiInt = Math.floor(ki) 
+    if (ki !== kiInt ){ 
+        return res.status(400).json({message: "solo se permite valores numericos"})
+    };
+    if (gender !== "male" || gender !== "female"){
+        return res.status(401).json({message: "solo se permiten añadir los generos Male y Female" })
+    };
     try{
-        const character=await Character.create (req.body);
-        res.status(201).json (character);
+        const character = await Character.create(req.body);
+        res.status(201).json(character);
     } catch (err){
         res.status(500).json({ error: err.message});
-    }
+    };
 };
 
 export const getAllcharacter = async(req, res) =>{
@@ -52,4 +79,3 @@ export const deletecharacter = async (req,res) =>{
             res.status(500).json({error: err.message});
         }
     };
-    
