589614e8de3bf313b3d984edc84a4f4b7599a128
¨lautaro <shortlauty35@gmail.com>
2025-06-26 10:18:46 -0300

    se corrigio un error en sequelize

--- src/models/Character.model.js
+++ src/models/Character.model.js
@@ -1,7 +1,8 @@
-import sequelize from "../config/database";
 import { DataTypes} from "sequelize";
+import sequelize from "../config/database.js";
 
 export const character=sequelize.define("character",{
+    id: {type: DataTypes.NUMBER, allownull: false},
     name: {type: DataTypes.STRING, allownull: false},
     ki: {type: DataTypes.NUMBER, allownull: false },
     gender:{type: DataTypes.STRING, allownull: false},
